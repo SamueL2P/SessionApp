4199db4cd31f55f2de649e09832733c60e580749
Samuel <padmadansamuel@gmail.com>
2024-09-06 20:23:15 +0530

    Add project files.

---
+++ SessionApp/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        private const string CorrectUsername = "testuser";
+        private const string CorrectPassword = "password123";
+
+   
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            
+            if (username == CorrectUsername && password == CorrectPassword)
+            {
+         
+                Session["User"] = username;
+                Session["LoginTime"] = DateTime.Now;
+
+            
+                return RedirectToAction("Dashboard", "Home");
+            }
+            else
+            {
+        
+                TempData["ErrorMessage"] = "Invalid username or password. Please try again.";
+                return RedirectToAction("Login");
+            }
+        
+             }
+    }
+}
---
+++ SessionApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Dashboard()
+        {
+       
+            if (Session["User"] == null)
+            {
+                TempData["ErrorMessage"] = "You must log in to access the dashboard.";
+                return RedirectToAction("Login", "Account");
+            }
+
+            ViewBag.User = Session["User"];
+            return View();
+        }
+        public ActionResult CheckSession()
+        {
+            if (Session["User"] == null)
+            {
+                TempData["ErrorMessage"] = "Session timed out. Please log in again.";
+                return RedirectToAction("Login", "Account");
+            }
+            
+            return View();
+        }
+    }
+}
